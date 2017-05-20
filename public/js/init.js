f8a58fe2fff8333d41f59f489ab9e8ae24120936
eyea <deyesea@gmail.com>
2017-05-20 23:57:52 +0800

    console change

---
+++ public/js/init.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  if(window.console){
+    var cons = console;
+    if(cons){
+      cons.log('%c\n ', 'padding:235px 30px; line-height:30px; background: url("/images/info.jpeg") no-repeat');
+      cons.log('博客请点击: \n\n%chttp://blog.eyeseau.com/\n\n^_^','color:red; font-weight:bold');
+    }
+  }
+}

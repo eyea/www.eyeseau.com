3b280da77761841df9aa5aed8bd8a6e366a0f5c9
eyea <deyesea@gmail.com>
2017-05-21 01:09:26 +0800

    add console.log text text-shadow

--- public/js/init.js
+++ public/js/init.js
@@ -2,8 +2,8 @@ window.onload = function(){
   if(window.console){
     var cons = console;
     if(cons){
-      cons.log('%c\n ', 'padding:235px 30px; line-height:30px; background: url("/images/info.jpeg") no-repeat');
-      cons.log('博客请点击: \n\n%chttp://blog.eyeseau.com/\n\n^_^','color:red; font-weight:bold');
+      cons.log('%c^_^  博客点击   ^_^ \n ', 'text-shadow: 0 0 10px #fff,0 0 20px #fff,0 0 30px #fff,0 0 40px #ff00de,0 0 70px #ff00de,0 0 80px #ff00de,0 0 100px #ff00de,0 0 150px #ff00de;');
+      cons.log('%chttp://blog.eyeseau.com/\n','color:red;');
     }
   }
 }

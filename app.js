d656a487c8c3297031aa978b2635d14d45c17880
eyea <deyesea@gmail.com>
2017-05-21 23:43:21 +0800

    dist

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'dist/images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/dist/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(__dirname + '/dist'));
 
 app.use('/', index);
 app.use('/users', users);
